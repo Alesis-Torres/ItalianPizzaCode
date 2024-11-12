9efa0e9d2bce57f174a3bd55954616bd948162af
LuisElizaldeX <king_mbea@hotmail.com>
2024-11-11 22:23:43 -0600

    Pruebas unitarias insumo, producto y receta

---
+++ Pruebas/PruebasDAOs/PruebaProductosDAO.cs
@@ -0,0 +1,26 @@
+﻿using ItalianPicza.DatabaseModel.DAO_s;
+using ItalianPicza.DatabaseModel.DataBaseMapping;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Transactions;
+
+namespace Pruebas.PruebasDAOs
+{
+    [TestClass]
+    public class PruebaProductosDAO
+    {
+        public static ProductosDAO productosDAO;
+
+        [TestMethod]
+        public void PruebaObtenerProductos()
+        {
+            using (var scope = new TransactionScope(TransactionScopeOption.Required, new TransactionOptions { IsolationLevel = IsolationLevel.ReadUncommitted }))
+            {
+                productosDAO = new ProductosDAO();
+                List<producto> listaProductos = productosDAO.ObtenerProductos();
+                Assert.IsTrue(listaProductos.Count > 0);
+            }
+        }
+    }
+}
